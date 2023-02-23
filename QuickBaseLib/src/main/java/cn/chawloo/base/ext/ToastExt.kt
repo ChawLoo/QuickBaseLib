29ceac6a92a3da19b4eb98cd40e4aec5ad81c75a
ChawLoo <530186472@qq.com>
2023-02-23 16:35:09 +0800

    1.优化气泡消息扩展类 2.发布1.1.15

--- QuickBaseLib/src/main/java/cn/chawloo/base/ext/ToastExt.kt
+++ QuickBaseLib/src/main/java/cn/chawloo/base/ext/ToastExt.kt
@@ -2,26 +2,12 @@
 
 package cn.chawloo.base.ext
 
-import android.content.Context
 import androidx.annotation.StringRes
-import androidx.fragment.app.Fragment
 import com.hjq.toast.Toaster
 
-fun Fragment.toast(message: CharSequence?) = message?.takeIf { it.isNotBlank() }?.let { requireActivity().toast(it) }
-fun Fragment.toast(@StringRes message: Int) = requireActivity().toast(message)
-fun Context.toast(message: CharSequence?) = message?.takeIf { it.isNotBlank() }?.let { Toaster.show(it) }
-fun Context.toast(@StringRes message: Int) = Toaster.show(message)
 fun toast(message: CharSequence?) = message?.takeIf { it.isNotBlank() }?.let { Toaster.show(it) }
 fun toast(@StringRes message: Int) = Toaster.show(message)
-fun Fragment.longToast(message: CharSequence?) = message?.takeIf { it.isNotBlank() }?.let { requireActivity().longToast(it) }
-fun Fragment.longToast(@StringRes message: Int) = requireActivity().longToast(message)
-fun Context.longToast(message: CharSequence?) = message?.takeIf { it.isNotBlank() }?.let { Toaster.showLong(it) }
-fun Context.longToast(@StringRes message: Int) = Toaster.showLong(message)
-fun longToast(message: CharSequence?) = message?.takeIf { it.isNotBlank() }?.let { Toaster.showLong(it) }
-fun longToast(@StringRes message: Int) = Toaster.showLong(message)
-fun Fragment.toast(t: Throwable?) = t?.message?.takeIf { it.isNotBlank() }?.let { requireActivity().toast(it) }
-fun Context.toast(t: Throwable?) = t?.message?.takeIf { it.isNotBlank() }?.let { Toaster.show(it) }
 fun toast(t: Throwable?) = t?.message?.takeIf { it.isNotBlank() }?.let { Toaster.show(it) }
-fun Fragment.longToast(t: Throwable?) = t?.message?.takeIf { it.isNotBlank() }?.let { requireActivity().longToast(it) }
-fun Context.longToast(t: Throwable?) = t?.message?.takeIf { it.isNotBlank() }?.let { Toaster.showLong(it) }
+fun longToast(@StringRes message: Int) = Toaster.showLong(message)
+fun longToast(message: CharSequence?) = message?.takeIf { it.isNotBlank() }?.let { Toaster.showLong(it) }
 fun longToast(t: Throwable?) = t?.message?.takeIf { it.isNotBlank() }?.let { Toaster.showLong(it) }
